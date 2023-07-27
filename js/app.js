4c9e09ee4e959cc655af67b1599dc4afb4468473
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-07-27 03:35:37 +0200

    je rentre comme Neo dans le programme

--- js/app.js
+++ js/app.js
@@ -4,6 +4,42 @@ let navLinks = document.querySelectorAll('header nav a');
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.querySelector('canvas'), ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var letters = "I";
+    letters = letters.split("");
+
+    var fontSize = 10, columns = canvas.width / fontSize;
+    var laps = 0;
+
+    var drops = [];
+    for(var i = 0; i < columns; i++) {
+        drops[i] = 1;
+    }
+
+    function draw() {
+        ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        for(var i = 0; i < drops.length; i++) {
+            var text = letters[Math.floor(Math.random() * letters.length)];
+            ctx.fillStyle = "#0f0";
+            ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+            if(drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
+                drops[i] = 0;
+                letters = "&#{[|)(]}BHGLMOI@!$";
+            }
+            drops[i]++;
+        }
+    }
+
+    setInterval(draw, 90);
+});
+
 window.onscroll = () => {
     sections.forEach(section => {
         let top = window.scrollY;
